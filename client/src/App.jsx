cc26d2c8d5b125fec2a7b6c042290a149277f858
ClaBoris <claudia.borsani14@gmail.com>
2024-04-27 11:46:54 +0200

    React

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import{BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from "./routes/Home"
+import ProductDetailPage from "./routes/ProductDetailPage"
+import UpdatePage from "./routes/UpdatePage"
 const App = () => {
-    return <div>App</div>
+    return <div>
+       <Router>
+            <Routes>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/products/:id/update" component={UpdatePage}/>
+            <Route exact path="/products/:id" component={ProductDetailPage}/>
+            </Routes>
+       </Router> 
+    </div>
 };
 
 export default App;
